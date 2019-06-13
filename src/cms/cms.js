da9d24d1c150a70665efdac7190cec92b300a9b1
Daniel Farrell <daniel.farrell@argos.co.uk>
2019-06-13 23:12:13 +0100

    Test adding homepage

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,12 @@
 import CMS from 'netlify-cms'
 
+import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
